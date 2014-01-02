ce9b68999cd5ed9d8b83d41551282425edb5382b
Gurudath Bn <gurudath.1989@gmail.com>
2014-01-02 19:31:30 +0530

    inicial commit

---
+++ Guardfile
@@ -0,0 +1,8 @@
+# A sample Guardfile
+# More info at https://github.com/guard/guard#readme
+
+
+guard 'puma', :port => 4000 do
+  watch('Gemfile.lock')
+  watch(%r{^config|lib/.*})
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4Project::Application.config.secret_key_base = '6848405c4bd29f0b9a58dc6ae5a703fee5fa08beceeb0e73b9b80dd03aca9215352de95fbfb3afd0ec58434826df6cb0e991348f56627fdd52646bf97e616c2a'
